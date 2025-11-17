4ff4637f4d7bbc310d4ada94c31485f3e4185334
Johnry0309 <johnryqadornado0309@gmail.com>
2025-11-17 22:08:44 +0800

    finals

--- komekain/settings.py
+++ komekain/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "change-me"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["komekainpage.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
